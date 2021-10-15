62fd1bdd663f1a031999e9dfa52adad70144ae82
cretu <i.cretu@swphl.de>
2021-10-15 12:35:18 +0200

    Adaugat un PostController find(1)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //
     public function index(){
+        $posts = Post::find(1);
         dd("in index");
     }
 }
