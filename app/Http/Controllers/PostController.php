fc4867369b237fb1088fa15df1374c1de2aac300
cretu <i.cretu@swphl.de>
2021-10-22 15:57:57 +0200

    Lectia 8 find(1), all(), where()

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,19 @@ class PostController extends Controller
 {
     //
     public function index(){
+       echo("fiind(1)");
         $post = Post::find(1);
-        dd($post->content);
+        dump($post->content);
+
+        echo("all()");
+        $posts = Post::all();
+                foreach ($posts as $post){
+            dump($post->content);
+        }
+        echo("where is publisched = 1");
+        $posts = Post::where("is_publisched", 1)->get();
+        foreach ($posts as $post){
+            dump($post->content);
+        }
     }
 }
